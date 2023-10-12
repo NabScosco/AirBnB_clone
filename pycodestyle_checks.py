2b616733a021ea22246075228fe61c2f77b86120
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2023-10-11 22:56:31 -0700

    A code to pass the pycode style

---
+++ pycodestyle_checks.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This file is to check if pycodestyle passes
+"""
+
+
+def hello():
+
+    "Prompt the user for their name"
+    name = input("Enter your name: ")
+
+    "prints out the name"
+    print(f"Hello! {name}, welcome to the AirBnB clone project")
+
+
+hello()
