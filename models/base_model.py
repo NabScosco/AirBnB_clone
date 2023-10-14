aeb2d128117a18d930ef0d53e9fe3648c30c6a5d
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2023-10-14 12:17:15 -0700

    Recreated the instance

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,7 @@ class BaseModel:
     It defines common attributes and methods.
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initialize a new instance of BaseModel.
 
@@ -20,9 +20,18 @@ class BaseModel:
             updated_at (datetime): Timestamp of when the instance is
             last updated.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if  kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        setattr(self, key, datetime.strptime( value,
+                            "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         """
