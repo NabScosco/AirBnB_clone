e22bf09cef0fcdceb12b460c25665d462d9ab79c
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2023-10-15 10:18:41 -0700

    task for done

--- models/base_model.py
+++ models/base_model.py
@@ -10,13 +10,26 @@ class BaseModel:
     """
     Defines common attributes/methods for other classes.
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializes a new instance.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, value)
+            if not hasattr(self, 'id'):
+                self.id = str(uuid.uuid4())
+            if not hasattr(self, 'created_at'):
+                self.created_at = datetime.now()
+            if not hasattr(self, 'updated_at'):
+                self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
