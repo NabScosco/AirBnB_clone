3efcd24ab57f7302e7a3a941519fc8c3e00edc3b
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2023-10-16 19:58:06 -0700

    made sure when you presss the enter key, It doesn't implement anything

--- console.py
+++ console.py
@@ -5,13 +5,17 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
-    def do_quit(self, arg):
+    def do_quit(self, line):
         """Quit command to exit the program"""
         return True
 
-    def do_EOF(self, arg):
+    def do_EOF(self, line):
         """Exit the program using Ctrl+D or EOF"""
         return True
+
+    def emptyline(self):
+        """Called when an empty line is entered"""
+        pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
