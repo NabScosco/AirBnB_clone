ce9dfb1f19803c2fad3bf9a7e59197a233ccefc1
Kingsley Fynn Asaam <kingsley.asaam@gmail.com>
2023-10-16 23:21:28 -0700

    added documentations and console.py

--- console.py
+++ console.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
 
 import cmd
+from models.base_model import BaseModel
+
 
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
-    def do_quit(self, line):
+    def do_quit(self, arg):
         """Quit command to exit the program"""
         return True
 
-    def do_EOF(self, line):
+    def do_EOF(self, arg):
         """Exit the program using Ctrl+D or EOF"""
         return True
 
     def emptyline(self):
         """Called when an empty line is entered"""
         pass
+
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
